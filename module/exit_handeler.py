b33eb56a9a70ac2e9c4f3f04cd5c03120c9c93fa
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-03-22 01:40:44 +0530

    fixed type issue

--- module/exit_handeler.py
+++ module/exit_handeler.py
@@ -2,10 +2,10 @@ import sys
 from .Credentials import Creditial
 from .logout import logout
 
-def exit_handler(credentials: Creditial, cred_index: int | None = None,  signal=None, frame=None):
+def exit_handler(cred_index: int | None, credentials: Creditial, signal=None, frame=None):
     """Handles exit by logging out and terminating the script."""
     print("\nExiting...")
-    if not cred_index:
+    if cred_index is None:
         for cred in credentials:
             logout(cred)
     else:
