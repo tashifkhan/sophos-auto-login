152c3bae1c53e6db88bea7c7ae0884cc5582f71b
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-04-10 13:23:07 +0530

    added check_internet_connection function

--- module/__init__.py
+++ module/__init__.py
@@ -4,5 +4,6 @@ from .exit_handeler import exit_handler
 from .wifi_name_extractor import get_wifi_name
 from .notification_handler import send_notification
 from .deamon_exit_handeler import main as deamon_exit
+from .check_internet import check_internet_connection
 
-__all__ = ["login", "logout", "exit_handler", "get_wifi_name", "deamon_exit", "send_notification"]
+__all__ = ["login", "logout", "exit_handler", "get_wifi_name", "deamon_exit", "send_notification", "check_internet_connection"]
